550cc4b1a0c85174541a095f57b7f0aa22856c60
Yanis Zeghiche <yanis.zeghiche@epitech.eu>
2023-02-11 15:52:49 +0100

    [+] Added bat logo

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import type { FunctionComponent, ReactNode } from "react";
+import { Link } from "react-router-dom";
 
 interface LayoutProps {
   children?: ReactNode;
@@ -9,7 +10,9 @@ const Layout: FunctionComponent<LayoutProps> = ({ children }) => {
     <div className="min-h-screen bg-[#121212] text-white">
       <div className="mx-auto grid min-h-screen w-full max-w-7xl grid-rows-[5rem,1fr,5rem] px-8">
         <header className="flex items-center justify-center">
-          MY_MINIGAMES
+          <Link to="/">
+            <img className="h-10" src="/images/logo_bat.png" alt="ZEBI" />
+          </Link>
         </header>
         <main className="my-6">{children}</main>
       </div>
