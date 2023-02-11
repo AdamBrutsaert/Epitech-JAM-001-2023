7b785d6584a222e3edab9ef59f7dd89b6a1db865
Yanis Zeghiche <yanis.zeghiche@epitech.eu>
2023-02-11 14:42:03 +0100

    [+] Added heros images

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,9 +8,10 @@ const Layout: FunctionComponent<LayoutProps> = ({ children }) => {
   return (
     <div className="min-h-screen bg-[#121212] text-white">
       <div className="mx-auto grid min-h-screen w-full max-w-7xl grid-rows-[5rem,1fr,5rem] px-8">
-        <header className="flex items-center justify-center">HEADER</header>
+        <header className="flex items-center justify-center">
+          MY_MINIGAMES
+        </header>
         <main className="my-8">{children}</main>
-        <footer className="flex items-center justify-center">FOOTER</footer>
       </div>
     </div>
   );
